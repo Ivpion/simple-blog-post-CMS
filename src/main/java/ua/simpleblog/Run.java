b9204eeef23a4cbf3f81efa573679ced707d5f6b
ivpion <ivanpiontkevich@gmail.com>
2018-03-26 20:49:19 +0300

    simple-blog-post-CMS-1 init Run

--- src/main/java/ua/simpleblog/Run.java
+++ src/main/java/ua/simpleblog/Run.java
@@ -1,4 +1,11 @@
 package ua.simpleblog;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Run {
+    public static void main(String[] args) {
+        SpringApplication.run(Run.class, args);
+    }
 }
